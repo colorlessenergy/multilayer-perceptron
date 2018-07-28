5f70f1cea44332069e4fd98c2ae693ef15b86d89
colorlessenergy <brianmunozdev@gmail.com>
2018-07-28 08:01:20 -0700

    implemented a neural network

--- main.js
+++ main.js
@@ -2,13 +2,40 @@ let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 
 let brain;
+
+let training_data = [
+  {
+    input: [0, 1],
+    targets: [1]
+  },
+  {
+    input: [1, 0],
+    targets: [1]
+  },
+  {
+    input: [0, 0],
+    targets: [0]
+  },
+  {
+    input: [1, 1],
+    targets: [0]
+  },
+]
 function setup () {
 
   brain = new NeuralNetwork(2, 2, 1);
-  let input = [1, 0];
-  let output = brain.feedforward(input)
-  console.log(output)
-  draw();
+
+  for (let i = 0; i < 10000; i++) {
+    for (data of training_data) {
+      brain.train(data.input, data.targets)
+    }
+  }
+
+  console.log(brain.feedforward([1, 0]))
+  console.log(brain.feedforward([0, 1]))
+  console.log(brain.feedforward([0, 0]))
+  console.log(brain.feedforward([1, 1]))
+
 }
 
 function draw() {
--- matrix.js
+++ matrix.js
@@ -1,12 +1,10 @@
-// let m  = new Matrix(3, 2)
-
 class Matrix {
   constructor(rows, cols) {
     this.rows = rows;
     this.cols = cols;
     this.data = [];
 
-    for (let i = 0;i < this.rows; i++) {
+    for (let i = 0; i < this.rows; i++) {
       this.data[i] = [];
       for (let j = 0; j < this.cols; j++) {
         this.data[i][j] = 0;
@@ -16,13 +14,21 @@ class Matrix {
 
   static fromArray(arr) {
     let m = new Matrix(arr.length, 1);
-
     for (let i = 0; i < arr.length; i++) {
       m.data[i][0] = arr[i];
     }
-
-    m.print()
     return m;
+  }
+
+  static subtract(a, b) {
+    // Return a new Matrix a-b
+    let result = new Matrix(a.rows, a.cols);
+    for (let i = 0; i < result.rows; i++) {
+      for (let j = 0; j < result.cols; j++) {
+        result.data[i][j] = a.data[i][j] - b.data[i][j];
+      }
+    }
+    return result;
   }
 
   toArray() {
@@ -38,47 +44,7 @@ class Matrix {
   randomize() {
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
-        this.data[i][j] = Math.floor(Math.random() * 2) -1;
-      }
-    }
-  }
-
-  transpose() {
-    let result = new Matrix(this.cols, this.rows);
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        result.data[j][i] = this.data[i][j];
-      }
-    }
-    return result;
-  }
-
-  static multiply(a, b) {
-    // matrix product
-    if (a.cols !== b.rows) {
-      console.log(' Cols of A must match rows of B');
-      return undefined;
-    }
-    let result = new Matrix(a.rows, b.cols);
-
-    for (let i = 0; i < result.rows; i++) {
-      for (let j= 0; j < result.cols; j++) {
-        // dot product of values in col
-        let sum = 0;
-        for (let k = 0; k < a.cols; k++) {
-          sum += a.data[i][k] * b.data[k][j]
-        }
-        result.data[i][j] = sum;
-      }
-    }
-    return result;
-  }
-
-  multiply(n) {
-  // scalar product
-    for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          this.data[i][j] *= n;
+        this.data[i][j] = Math.random() * 2 - 1;
       }
     }
   }
@@ -99,18 +65,77 @@ class Matrix {
     }
   }
 
-  map(fn) {
-    // apply a function to every element of matrix
+  static transpose(matrix) {
+    let result = new Matrix(matrix.cols, matrix.rows);
+    for (let i = 0; i < matrix.rows; i++) {
+      for (let j = 0; j < matrix.cols; j++) {
+        result.data[j][i] = matrix.data[i][j];
+      }
+    }
+    return result;
+  }
+
+  static multiply(a, b) {
+    // Matrix product
+    if (a.cols !== b.rows) {
+      console.log('Columns of A must match rows of B.')
+      return undefined;
+    }
+    let result = new Matrix(a.rows, b.cols);
+    for (let i = 0; i < result.rows; i++) {
+      for (let j = 0; j < result.cols; j++) {
+        // Dot product of values in col
+        let sum = 0;
+        for (let k = 0; k < a.cols; k++) {
+          sum += a.data[i][k] * b.data[k][j];
+        }
+        result.data[i][j] = sum;
+      }
+    }
+    return result;
+  }
+
+  multiply(n) {
+    if (n instanceof Matrix) {
+      // hadamard product
       for (let i = 0; i < this.rows; i++) {
-          for (let j = 0; j < this.cols; j++) {
-            let val = this.data[i][j];
-            this.data[i][j] = fn(val);
+        for (let j = 0; j < this.cols; j++) {
+          this.data[i][j] *= n.data[i][j];
+        }
+      }
+    } else {
+      // Scalar product
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          this.data[i][j] *= n;
         }
       }
     }
-
-  print() {
-    console.table(this.data)
   }
 
+  map(func) {
+    // Apply a function to every element of matrix
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let val = this.data[i][j];
+        this.data[i][j] = func(val);
+      }
+    }
+  }
+
+  static map(matrix, func) {
+    let result = new Matrix(matrix.rows, matrix.cols);
+    // Apply a function to every element of matrix
+    for (let i = 0; i < matrix.rows; i++) {
+      for (let j = 0; j < matrix.cols; j++) {
+        let val = matrix.data[i][j];
+        result.data[i][j] = func(val);
+      }
+    }
+    return result;
+  }
+
+  print() {
+    console.table(this.data);
+  }
 }

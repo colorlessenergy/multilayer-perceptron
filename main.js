3ff6c403816f7fa141ca341ea59601dae6552fcb
colorlessenergy <brianmunozdev@gmail.com>
2018-07-28 06:17:30 -0700

    add feedforward function for neural network

--- main.js
+++ main.js
@@ -4,27 +4,10 @@ let ctx = canvas.getContext('2d');
 let brain;
 function setup () {
 
-  brain = new NeuralNetwork(3, 3, 1);
-
-  let a = new Matrix(2,3);
-  a.randomize();
-  a.print();
-
-  function doubleIt(x) {
-    return x * 2;
-  }
-
-  a.map(doubleIt);
-  a.print();
-
-  // let b = new Matrix(3, 2);
-  // b.randomize();
-  // console.table(a.matrix);
-  // console.table(b.matrix);
-
-  // let c = a.multiply(b);
-
-  // console.table(c.matrix)
+  brain = new NeuralNetwork(2, 2, 1);
+  let input = [1, 0];
+  let output = brain.feedforward(input)
+  console.log(output)
   draw();
 }
 
--- nn.js
+++ nn.js
@@ -1,14 +1,44 @@
+function sigmoid(x) {
+  return 1 / (1 + Math.exp(-x))
+}
+
 class NeuralNetwork {
   constructor (input_nodes, hiddens_nodes, output_nodes) {
     this.input_nodes = input_nodes;
     this.hiddens_nodes = hiddens_nodes;
     this.output_nodes = output_nodes;
+
+    this.weights_ih = new Matrix(this.hiddens_nodes, this.input_nodes);
+    this.weights_ho = new Matrix(this.output_nodes, this.input_nodes);
+    this.weights_ih.randomize();
+    this.weights_ho.randomize();
+
+    this.bias_h = new Matrix(this.hiddens_nodes, 1);
+    this.bias_o = new Matrix(this.output_nodes, 1);
+    this.bias_h.randomize();
+    this.bias_o.randomize();
   }
 
-  feedforward(input) {
+  feedforward(input_array) {
 
-    // LOTS OF MATRIX MATH!!
+    // generating the hidden outputs
+    let inputs = Matrix.fromArray(input_array);
+    let hidden = Matrix.multiply(this.weights_ih, inputs);
+    hidden.add(this.bias_h);
+    // activation function
 
-    return guess;
+    hidden.map(sigmoid);
+
+    // Generating the output's output
+    let output = Matrix.multiply(this.weights_ho, hidden);
+    output.add(this.bias_o);
+    output.map(sigmoid);
+    
+    // sending it back
+    return output.toArray();
+  }
+
+  train (inputs, answer) {
+    
   }
 }
